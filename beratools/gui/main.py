e6d8566e9143407d91c9089bc3dd4461b152ebf7
Richard <502763+RichardQZeng@users.noreply.github.com>
2025-10-27 15:55:36 -0600

    Update Anaconda workflow and clean main.py imports

--- beratools/gui/main.py
+++ beratools/gui/main.py
@@ -13,14 +13,6 @@ Description:
     Main entry point of BERA Tools.
 """
 
-import sys
-from pathlib import Path
-
-# if __name__ == "__main__":
-#     current_file = Path(__file__).resolve()
-#     btool_dir = current_file.parents[2]
-#     sys.path.insert(0, btool_dir.as_posix())
-
 from beratools.gui.bt_gui_main import runner
 
 
